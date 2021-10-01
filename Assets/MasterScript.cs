cbdd5e448ab24e8297301aff9f4c2e45546b9da7
Jack Carroll <boomkiller1119@gmail.com>
2021-10-02 07:49:17 +0800

    October 2, 2021 7:49 AM

--- Assets/MasterScript.cs
+++ Assets/MasterScript.cs
@@ -22,11 +22,22 @@ public class MasterScript : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Force, 0f));
+                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Force * Time.deltaTime, 0f));
             }
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
-                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Force, 0f));
+                player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Force * Time.deltaTime, 0f));
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                ground.GetComponent<Rigidbody2D>().AddForce(new Vector2(-Force * Time.deltaTime, 0f));
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                ground.GetComponent<Rigidbody2D>().AddForce(new Vector2(Force * Time.deltaTime, 0f));
             }
         }
     }
