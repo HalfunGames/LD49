6598c78d246a36dd26a3b4901180ebb0de68cd16
Jack Carroll <boomkiller1119@gmail.com>
2021-10-02 08:21:55 +0800

    October 2, 2021 8:21 AM

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -6,10 +6,11 @@ public class Follow : MonoBehaviour
 {
     public GameObject Target;
     public Vector3 Offset;
+    public float LagConstant;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Target.transform.position + Offset;
+        transform.position += (transform.position - (Target.transform.position + Offset)) * Time.deltaTime * LagConstant;
     }
 }
