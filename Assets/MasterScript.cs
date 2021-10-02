39bbe30214a1f73432b1c52ab924f4712f574b4b
Jack Carroll <boomkiller1119@gmail.com>
2021-10-02 10:40:39 +0800

    October 2, 2021 10:40 AM

--- Assets/MasterScript.cs
+++ Assets/MasterScript.cs
@@ -27,7 +27,6 @@ public class MasterScript : MonoBehaviour
     {
         TargetValue = 0f;
         ControlValue = 1f;
-        yield return new WaitForSeconds(1.5f);
         CoroutineStop = false;
         while (!CoroutineStop)
         {
